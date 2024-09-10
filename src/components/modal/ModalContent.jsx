c8b83a0c41ebebab8037a21d34c85e42f3dfd0bd
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-09-10 22:13:41 +0600

    add child component

--- src/components/modal/ModalContent.jsx
+++ src/components/modal/ModalContent.jsx
@@ -4,6 +4,7 @@ const ModalContent = ({onClose}) => {
   return (
     <div className="flex justify-evenly items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black text-white p-2" role="dialog" aria-modal="true" >
          <p className='mx-2'>Hey I am a Modal</p>
+       
          <button
              className="bg-blue-400 text-white p-1 border rounded-md"
             onClick={onClose}>Close</button>
@@ -11,4 +12,4 @@ const ModalContent = ({onClose}) => {
   )
 }
 
-export default ModalContent
+export default ModalContent;
--- src/components/traditional-modal/ModalContentOne.jsx
+++ src/components/traditional-modal/ModalContentOne.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+const Child = () => {
+  return(
+    <div className='border border-red-500 p-1'>
+      <h1>I am a child</h1>
+      <button>Click</button>
+    </div>
+  )
+}
+
 const ModalContentOne = ({onClose}) => {
   return (
     <div className="flex justify-evenly items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black text-white p-2" >
          <p className='mx-2'>Hey I am a Modal</p>
+         <Child />
          <button
              className="bg-blue-400 text-white p-1 border rounded-md"
             onClick={onClose}>Close</button>
