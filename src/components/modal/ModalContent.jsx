c1a7fc1d9f23b8db43d31a24a89ba0d38f6a8326
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-09-10 20:50:46 +0600

    Portal is working

--- src/components/modal/ModalContent.jsx
+++ src/components/modal/ModalContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ModalContent = ({onClose}) => {
   return (
-    <div className="flex justify-evenly items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black text-white p-2" >
+    <div className="flex justify-evenly items-center border rounded-lg absolute top-20 left-12 bottom-20 z-10 bg-black text-white p-2" role="dialog" aria-modal="true" >
          <p className='mx-2'>Hey I am a Modal</p>
          <button
              className="bg-blue-400 text-white p-1 border rounded-md"
